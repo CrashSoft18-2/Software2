68bd768cf9a1215748a39a3c55a1666242159f30
erojass <e.rojas1096@gmail.com>
2018-09-24 11:44:38 -0500

    fix list

--- helloFlask.py
+++ helloFlask.py
@@ -34,6 +34,7 @@ def do_the_login():
 	password = ""
 	print (request.form['uname'])
 	print (request.form['psw']) 
+	print(lista_alumnos)
 	for key, value in lista_alumnos.items():
 		usuario = str(value.get("user"))
 		password = str(value.get("password"))
