f5202dc35fc389f1fd3d1f404e8d7ce38eff9e0b
Marcelo <marcelocoro17@gmail.com>
2018-09-25 18:12:45 -0500

    Mostrar Citas 2

--- queries/queryCitas.sql
+++ queries/queryCitas.sql
@@ -1,12 +1,10 @@
-SELECT usuario.nombre, usuario.apellido, carrera.nombre, curso.nombre, cita.Fecha, asesorias.Hora, asesorias.lugar, consultas.consulta
+SELECT usuario.nombre, usuario.apellido, carrera.nombre, curso.nombre, cita.Fecha, asesorias.Hora, asesorias.lugar 
 FROM cita
-INNER JOIN asesorias ON (asesorias.idAsesorias = cita.idAsesoriaProfeso) 
-INNER JOIN asesoriaprofesor ON ( asesorias.idAsesorias = asesoriaprofesor.idAsesorias )
+INNER JOIN asesoriaprofesor ON ( cita.idAsesoriaProfeso = asesoriaprofesor.idAsesorias )
+INNER JOIN asesorias ON (asesorias.idAsesorias = asesoriaprofesor.idAsesorias) 
 INNER JOIN profesor ON ( profesor.idProfesor = asesoriaprofesor.idProfesor )
 INNER JOIN usuario ON ( usuario.idUsuario = profesor.usuario_idUsuario )
-INNER JOIN profesorcurso ON (profesor.idProfesor = profesorcurso.idProfesor)
-INNER JOIN curso ON ( curso.idCurso = profesorcurso.idCurso)
+INNER JOIN curso ON ( curso.idCurso = cita.idCurso)
 INNER JOIN profesorcarrera ON ( profesor.idProfesor = profesorcarrera.idProfesor)
-INNER JOIN carrera ON ( carrera.idCarrera = profesorcarrera.idCarrera)
-INNER JOIN consultas ON ( consultas.idCita = cita.idCita )
+INNER JOIN carrera ON ( carrera.idCarrera = curso.Carrera_idCarrera)
 ;
